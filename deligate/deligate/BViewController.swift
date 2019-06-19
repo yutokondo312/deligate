e8c0834e8acc58c16f62410072512b60557335e5
yutokondo312 <51872518+yutokondo312@users.noreply.github.com>
2019-06-19 19:41:51 +0900

    New Push

---
+++ deligate/deligate/BViewController.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  deligate
+//
+//  Created by 近藤優斗 on 2019/06/16.
+//  Copyright © 2019年 kondoyuto. All rights reserved.
+//
+import UIKit
+import Foundation
+
+class BViewController: UIViewController {
+    var b =  ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
---
+++ deligate/deligate/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  deligate
+//
+//  Created by 近藤優斗 on 2019/06/16.
+//  Copyright © 2019年 kondoyuto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let a: String = "A"
+    
+ 
+    @IBAction func move2(_ sender: Any) {
+        let storyboard: UIStoryboard = self.storyboard!
+        if let second = storyboard.instantiateViewController(withIdentifier:"B") as? BViewController {
+            second.b = a
+        self.present(second, animated: true, completion: nil)
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
